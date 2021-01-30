4c54c21b8e6fd531ea31515302ca89ab243b7076
Jorge Vergara <jvc.9109@hotmail.com>
2021-01-30 16:33:43 +0100

    corrupt

---
+++ read-files/cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/jvc9109/go-first-app/read-files/internal/cli"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{Use: "beers-cli"}
+	rootCmd.AddCommand(cli.InitBeersCmd())
+	rootCmd.Execute()
+}
---
+++ read-files/internal/beer.go
@@ -0,0 +1,11 @@
+package beerscli
+
+type Beer struct {
+	productID int
+	Name      string
+	Price     string
+	Category  string
+	Type      string
+	Brewer    string
+	Country   string
+}
