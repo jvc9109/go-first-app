c4d8c18c7922af083b38d29bdb1cf85165a6ee0d
Jorge Vergara <jvc.9109@hotmail.com>
2021-01-30 16:44:39 +0100

    data model with repository

--- data-model/cmd/beers-cli/main.go
+++ data-model/cmd/beers-cli/main.go
@@ -1,2 +1,15 @@
 package main
 
+import (
+	"github.com/jvc9109/go-first-app/data-model/internal/cli"
+	"github.com/jvc9109/go-first-app/data-model/internal/storage/csv"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	csvRepo := csv.NewRepository()
+
+	rootCmd := &cobra.Command{Use: "beers-cli"}
+	rootCmd.AddCommand(cli.InitBeersCmd(csvRepo))
+	rootCmd.Execute()
+}
